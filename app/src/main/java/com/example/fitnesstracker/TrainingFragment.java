30f4debde9f2da6269adb649db77d3b1c343829c
Philipp <philipp.eckhard.staudinger@gmail.com>
2025-01-27 14:56:54 +0100

    setup basic structure
    Setup bottom nav-bar & different fragments for each tab

---
+++ app/src/main/java/com/example/fitnesstracker/TrainingFragment.java
@@ -0,0 +1,20 @@
+package com.example.fitnesstracker;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+import androidx.fragment.app.Fragment;
+
+public class TrainingFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Layout für das Fragment laden
+        View view = inflater.inflate(R.layout.fragment_training, container, false);
+
+        return view;
+    }
+}
