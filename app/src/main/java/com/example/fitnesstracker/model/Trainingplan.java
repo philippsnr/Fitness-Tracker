6f992b5ff83dc34c798fb1348c226f97ac5b714e
Nils897 <161706814+Nils897@users.noreply.github.com>
2025-02-12 14:35:06 +0100

    Created Classes
    Classes for table "Trainingplan" created and implemented Trainingplan

---
+++ app/src/main/java/com/example/fitnesstracker/model/Trainingplan.java
@@ -0,0 +1,29 @@
+package com.example.fitnesstracker.model;
+
+public class Trainingplan {
+    private final int id;
+    private String name;
+    private boolean isActive;
+    public Trainingplan(int id, String name, int isActive) {
+        this.id = id;
+        this.name = name;
+        this.isActive = (isActive == 1);
+    }
+    public Trainingplan(int id, String name, boolean isActive)
+    {
+        this.id = id;
+        this.name = name;
+        this.isActive = isActive;
+    }
+    
+    //Getter
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public boolean getIsActive() { return isActive; }
+
+    //Setter
+    public void setName(String name) { this.name = name; }
+    public void setIsActive(boolean isActive) {this.isActive = isActive; }
+
+    public int getIsActiveAsInt() { return isActive ? 1 : 0; }
+}
