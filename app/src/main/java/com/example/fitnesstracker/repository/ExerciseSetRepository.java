afde6b11d19e13bbea35a3f4b5b416508dd9b6f2
Nils897 <161706814+Nils897@users.noreply.github.com>
2025-02-12 13:18:26 +0100

    Update ExerciseSetRepository.java
    corrected method getLastSets

--- app/src/main/java/com/example/fitnesstracker/repository/ExerciseSetRepository.java
+++ app/src/main/java/com/example/fitnesstracker/repository/ExerciseSetRepository.java
@@ -1,14 +1,10 @@
 package com.example.fitnesstracker.repository;
 
 import com.example.fitnesstracker.model.ExerciseSet;
-
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-
 import com.example.fitnesstracker.database.DatabaseHelper;
-import com.example.fitnesstracker.model.MuscleGroup;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,17 +12,20 @@ public class ExerciseSetRepository
 {
     private final DatabaseHelper dbHelper;
 
-    public ExerciseSetRepository(Context context) { this.dbHelper = dbHelper; }
+    public ExerciseSetRepository(Context context) { this.dbHelper = new DatabaseHelper(context); }
 
-    public List<ExerciseSet> getLastSets()
+    public List<ExerciseSet> getLastSets(int trainingdayExerciseAssignmentId)
     {
         List<ExerciseSet> lastSets = new ArrayList<ExerciseSet>();
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        int trainingdayExerciseAssignment_id = ExerciseSet.getTrainingdayExerciseAssignment_id();
-        Cursor cursor = db.rawQuery("SELECT * FROM ExerciseSet WHERE trainingdayExerciseAssigment_id = ? ORDER BY date LIMIT 3", trainingdayExerciseAssignment_id);
+        Cursor cursor = db.rawQuery("SELECT * FROM ExerciseSet WHERE trainingdayExerciseAssignment_id = ? ORDER BY date DESC LIMIT 3", new String[]{String.valueOf(trainingdayExerciseAssignmentId)});
         if (cursor.moveToFirst()) {
             do {
-               
+                int id = cursor.getInt(cursor.getColumnIndexOrThrow("id"));
+                int setNumber = cursor.getInt(cursor.getColumnIndexOrThrow("setNumber"));
+                int repetition = cursor.getInt(cursor.getColumnIndexOrThrow("repetition"));
+                int weight = cursor.getInt(cursor.getColumnIndexOrThrow("weight"));
+                lastSets.add(new ExerciseSet(id, trainingdayExerciseAssignmentId, setNumber, repetition, weight));
             } while (cursor.moveToNext());
         }
         cursor.close();
