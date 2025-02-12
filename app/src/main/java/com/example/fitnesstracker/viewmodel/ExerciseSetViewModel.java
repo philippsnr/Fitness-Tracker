9820ae58c9cf82144d9dd37619e8782b7490c9fe
Nils897 <fleschhut.nils@gmail.com>
2025-02-12 11:12:03 +0100

    first code in Class ExerciseSetViewModel

--- app/src/main/java/com/example/fitnesstracker/viewmodel/ExerciseSetViewModel.java
+++ app/src/main/java/com/example/fitnesstracker/viewmodel/ExerciseSetViewModel.java
@@ -1,4 +1,15 @@
 package com.example.fitnesstracker.viewmodel;
 
-public class ExerciseSetViewModel {
+import android.app.Application;
+
+import androidx.lifecycle.AndroidViewModel;
+
+import com.example.fitnesstracker.repository.ExerciseSetRepository;
+
+public class ExerciseSetViewModel extends AndroidViewModel {
+    private final ExerciseSetRepository repository;
+    public ExerciseMuscleGroupViewModel(Application application) {
+        super(application);
+        repository = new ExerciseSetRepository(application);
+    }
 }
