50082cdd4c18102973f010da4c03ae084ac5dfb5
Nils897 <fleschhut.nils@gmail.com>
2025-02-19 16:15:14 +0100

    update ExerciseSet classes
    Added date variable in java classes and in the db creation_script

--- app/src/main/java/com/example/fitnesstracker/model/ExerciseSet.java
+++ app/src/main/java/com/example/fitnesstracker/model/ExerciseSet.java
@@ -1,18 +1,30 @@
 package com.example.fitnesstracker.model;
 
+import java.time.LocalDate;
+
 public class ExerciseSet {
     private final int id;
     private final int TrainingdayExerciseAssignment_id;
     private int setNumber;
     private int repetition;
     private int weight;
+    private final LocalDate date;
 
-    public ExerciseSet(int id, int trainingdayExerciseAssignment_id, int setNumber, int repetition, int weight) {
+    public ExerciseSet(int id, int trainingdayExerciseAssignment_id, int setNumber, int repetition, int weight, LocalDate date) {
         this.id = id;
         this.TrainingdayExerciseAssignment_id = trainingdayExerciseAssignment_id;
         this.setNumber = setNumber;
         this.repetition = repetition;
         this.weight = weight;
+        this.date = date;
+    }
+    public ExerciseSet(int id, int trainingdayExerciseAssignment_id, int setNumber, int repetition, int weight, String date) {
+        this.id = id;
+        this.TrainingdayExerciseAssignment_id = trainingdayExerciseAssignment_id;
+        this.setNumber = setNumber;
+        this.repetition = repetition;
+        this.weight = weight;
+        this.date = LocalDate.parse(date);
     }
     //Getter
     public int getId() { return id; }
@@ -20,6 +32,7 @@ public class ExerciseSet {
     public int getSetNumber() { return setNumber; }
     public int getRepetition() { return repetition; }
     public int getWeight() { return weight; }
+    public LocalDate getDate() { return date; }
 
     //Setter
     public void setSetNumber(int setNumber) { this.setNumber = setNumber; }
