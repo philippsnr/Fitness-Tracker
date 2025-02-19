b035271ba573459a92f0b629f22a5b1537087fa2
Philipp <philipp.eckhard.staudinger@gmail.com>
2025-02-19 16:49:56 +0100

    update TrainingplanViewModel

--- app/src/main/java/com/example/fitnesstracker/viewmodel/TrainingplanViewModel.java
+++ app/src/main/java/com/example/fitnesstracker/viewmodel/TrainingplanViewModel.java
@@ -1,8 +1,6 @@
 package com.example.fitnesstracker.viewmodel;
 
 import android.app.Application;
-import android.os.Handler;
-import android.os.Looper;
 
 import com.example.fitnesstracker.model.Trainingplan;
 import com.example.fitnesstracker.repository.TrainingplanRepository;
@@ -10,49 +8,42 @@ import com.example.fitnesstracker.repository.TrainingplanRepository;
 import java.util.List;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.function.Consumer;
 
 public class TrainingplanViewModel {
     private final TrainingplanRepository repository;
-    private List<Trainingplan> trainingplans;
     private final ExecutorService executorService;
-    private final Handler mainHandler;
 
     public TrainingplanViewModel(Application application) {
         repository = new TrainingplanRepository(application.getApplicationContext());
         executorService = Executors.newSingleThreadExecutor();
-        mainHandler = new Handler(Looper.getMainLooper());
     }
 
-    public List<Trainingplan> getTrainingplans() {
-        if (trainingplans == null) {
-            loadTrainingplans();
-        }
-        return trainingplans;
-    }
-
-    private void loadTrainingplans() {
-        executorService.execute(new Runnable() {
-            @Override
-            public void run() {
-                List<Trainingplan> plans = repository.getAllTrainingplans();
-                mainHandler.post(new Runnable() {
-                    @Override
-                    public void run() {
-                        trainingplans = plans;
-                    }
-                });
-            }
+    public void loadTrainingplans(Consumer<List<Trainingplan>> callback) {
+        executorService.execute(() -> {
+            List<Trainingplan> plans = repository.getAllTrainingplans();
+            callback.accept(plans); // Daten an UI weitergeben
         });
     }
-    public void addTrainingplan(Trainingplan trainingplan) {
-        repository.addTrainingplan(trainingplan);
+
+    public void addTrainingplan(Trainingplan trainingplan, Runnable onComplete) {
+        executorService.execute(() -> {
+            repository.addTrainingplan(trainingplan);
+            onComplete.run(); // Callback nach Abschluss
+        });
     }
 
-    public void updateTrainingplan(Trainingplan trainingplan) {
-        repository.updateTrainingplan(trainingplan);
+    public void updateTrainingplan(Trainingplan trainingplan, Runnable onComplete) {
+        executorService.execute(() -> {
+            repository.updateTrainingplan(trainingplan);
+            onComplete.run();
+        });
     }
 
-    public void deleteTrainingplan(Trainingplan trainingplan) {
-        repository.deleteTrainingplan(trainingplan);
+    public void deleteTrainingplan(Trainingplan trainingplan, Runnable onComplete) {
+        executorService.execute(() -> {
+            repository.deleteTrainingplan(trainingplan);
+            onComplete.run();
+        });
     }
 }
