6d54986ed76873d3e4d9938f48ca2d871ab082ca
Nils897 <fleschhut.nils@gmail.com>
2025-02-19 15:21:24 +0100

    updated Classes for table trainingplan

--- app/src/main/java/com/example/fitnesstracker/model/Trainingplan.java
+++ app/src/main/java/com/example/fitnesstracker/model/Trainingplan.java
@@ -1,17 +1,16 @@
 package com.example.fitnesstracker.model;
 
 public class Trainingplan {
-    private final int id;
+    private int id;
     private String name;
     private boolean isActive;
-    public Trainingplan(int id, String name, int isActive) { // Constructor Overload since SQLite doesnt support booleans
+    public Trainingplan(int id, String name, int isActive) { // Constructor Overload since SQLite doesn't support booleans
         this.id = id;
         this.name = name;
         this.isActive = (isActive == 1);
     }
-    public Trainingplan(int id, String name, boolean isActive)
+    public Trainingplan(String name, boolean isActive)
     {
-        this.id = id;
         this.name = name;
         this.isActive = isActive;
     }
@@ -24,6 +23,7 @@ public class Trainingplan {
     //Setter
     public void setName(String name) { this.name = name; }
     public void setIsActive(boolean isActive) {this.isActive = isActive; }
-
     public int getIsActiveAsInt() { return isActive ? 1 : 0; }
+
+    public void setId(long id) { this.id = (int)id; }
 }
--- app/src/main/java/com/example/fitnesstracker/repository/TrainingplanRepository.java
+++ app/src/main/java/com/example/fitnesstracker/repository/TrainingplanRepository.java
@@ -1,9 +1,9 @@
 package com.example.fitnesstracker.repository;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
-import android.provider.ContactsContract;
 
 import com.example.fitnesstracker.database.DatabaseHelper;
 import com.example.fitnesstracker.model.Trainingplan;
@@ -15,7 +15,7 @@ public class TrainingplanRepository {
     private final DatabaseHelper dbHelper;
 
     public TrainingplanRepository(Context context) { this.dbHelper = new DatabaseHelper(context); }
-    public List<Trainingplan> getTrainingplans()
+    public List<Trainingplan> getAllTrainingplans()
     {
         List<Trainingplan> trainingplans = new ArrayList<Trainingplan>();
         SQLiteDatabase db = dbHelper.getReadableDatabase();
@@ -31,5 +31,33 @@ public class TrainingplanRepository {
         cursor.close();
         db.close();
         return trainingplans;
+    }
+
+    public void addTrainingplan(Trainingplan trainingplan)
+    {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("name", trainingplan.getName());
+        values.put("isActive", trainingplan.getIsActiveAsInt());
+        long id = db.insert("Trainingplan", null, values);
+        trainingplan.setId(id);
+        db.close();
+    }
+    public void updateTrainingplan(Trainingplan trainingplan) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        ContentValues values = new ContentValues();
+        values.put("name", trainingplan.getName());
+        values.put("isActive", trainingplan.getIsActiveAsInt());  // Umwandlung von boolean zu int (1 oder 0)
+        String whereClause = "id = ?";
+        String[] whereArgs = { String.valueOf(trainingplan.getId()) };
+        db.update("Trainingplan", values, whereClause, whereArgs);
+        db.close();
+    }
+    public void deleteTrainingplan(Trainingplan trainingplan) {
+        SQLiteDatabase db = dbHelper.getWritableDatabase();
+        String whereClause = "id = ?";
+        String[] whereArgs = { String.valueOf(trainingplan.getId()) };
+        db.delete("Trainingplan", whereClause, whereArgs);
+        db.close();
     }
 }
