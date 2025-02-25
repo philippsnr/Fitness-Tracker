75cd704b59f0ac05df5606b758989b24fbf782e5
Philipp <philipp.eckhard.staudinger@gmail.com>
2025-02-25 08:55:02 +0100

    create function get all user infos

--- app/src/main/java/com/example/fitnesstracker/repository/UserInformationRepository.java
+++ app/src/main/java/com/example/fitnesstracker/repository/UserInformationRepository.java
@@ -7,12 +7,41 @@ import android.database.sqlite.SQLiteDatabase;
 import com.example.fitnesstracker.database.DatabaseHelper;
 import com.example.fitnesstracker.model.UserInformation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class UserInformationRepository {
     private final DatabaseHelper dbHelper;
 
     public UserInformationRepository(Context context) {
         this.dbHelper = new DatabaseHelper(context);
     }
+
+    // Alle gespeicherten User-Informationen abrufen
+    public List<UserInformation> getAllUserInformation() {
+        List<UserInformation> userInfoList = new ArrayList<>();
+        SQLiteDatabase db = dbHelper.getReadableDatabase();
+        Cursor cursor = db.rawQuery("SELECT * FROM UserInformation ORDER BY date DESC", null);
+
+        if (cursor.moveToFirst()) {
+            do {
+                UserInformation userInfo = new UserInformation(
+                        cursor.getInt(0),
+                        cursor.getInt(1),
+                        cursor.getString(2),
+                        cursor.getInt(3),
+                        cursor.getInt(4),
+                        cursor.getInt(5)
+                );
+                userInfoList.add(userInfo);
+            } while (cursor.moveToNext());
+        }
+
+        cursor.close();
+        db.close();
+        return userInfoList;
+    }
+
 
     // **Letzte gespeicherte User-Information abrufen**
     public UserInformation getLatestUserInformation() {
