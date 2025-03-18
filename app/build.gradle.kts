ad9a6e95292730e7f2ba887b7f2beb627159b7a1
Philipp <philipp.eckhard.staudinger@gmail.com>
2025-03-18 15:04:58 +0100

    fix main activity & gradle build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,33 +32,14 @@ android {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.activity:activity-ktx:1.8.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.firebase:firebase-crashlytics-buildtools:2.9.9")
-
-    // MPAndroidChart für Diagramme
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    implementation(libs.activity)
+    implementation(libs.constraintlayout)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
     implementation("com.github.PhilJay:MPAndroidChart:v3.1.0")
-
-    // Lokale Unit Tests (laufen auf der JVM)
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("com.google.truth:truth:1.1.3")
-    testImplementation("org.robolectric:robolectric:4.10.3")
-    testImplementation("androidx.arch.core:core-testing:2.2.0")
-    testImplementation("androidx.fragment:fragment-testing:1.6.2")
-    testImplementation("org.mockito:mockito-core:4.8.0")
-
-    // Instrumented Tests (laufen auf Android-Gerät/Emulator)
-    androidTestImplementation("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("androidx.test:runner:1.5.2")
-    androidTestImplementation("androidx.test:rules:1.5.0")
-    androidTestImplementation("androidx.fragment:fragment-testing:1.6.2") // Für FragmentScenario
-    androidTestImplementation("androidx.arch.core:core-testing:2.2.0")
-    androidTestImplementation("org.mockito:mockito-android:4.8.0")
-
-    // Jackson für JSON Verarbeitung
     implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
     implementation("com.fasterxml.jackson.core:jackson-annotations:2.15.2")
 }
