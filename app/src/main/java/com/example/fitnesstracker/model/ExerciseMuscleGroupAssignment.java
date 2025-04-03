61c534a0d152c38807b1cf9e0c53c38a286e6d42
Nils897 <161706814+Nils897@users.noreply.github.com>
2025-04-03 16:55:05 +0200

    create docstrings

--- app/src/main/java/com/example/fitnesstracker/model/ExerciseMuscleGroupAssignment.java
+++ app/src/main/java/com/example/fitnesstracker/model/ExerciseMuscleGroupAssignment.java
@@ -1,26 +1,55 @@
 package com.example.fitnesstracker.model;
 
+/**
+ * Represents the association between an exercise and a muscle group.
+ * This class models the many-to-many relationship between exercises
+ * and muscle groups in the fitness tracking system.
+ */
 public class ExerciseMuscleGroupAssignment {
     private int id;
     private int exerciseId;
     private int muscleGroupId;
 
+    /**
+     * Default constructor for creating an empty assignment.
+     * Primarily used by persistence frameworks.
+     */
     public ExerciseMuscleGroupAssignment() {}
 
+    /**
+     * Constructs a new exercise-muscle group assignment with the given IDs.
+     *
+     * @param exerciseId    the ID of the associated exercise
+     * @param muscleGroupId the ID of the associated muscle group
+     */
     public ExerciseMuscleGroupAssignment(int exerciseId, int muscleGroupId) {
         this.exerciseId = exerciseId;
         this.muscleGroupId = muscleGroupId;
     }
 
-    // Getter
+    /**
+     * Gets the unique identifier of this assignment.
+     *
+     * @return the assignment ID
+     */
     public int getId() {
         return id;
     }
 
+    /**
+     * Gets the ID of the associated exercise.
+     *
+     * @return the exercise ID
+     */
     public int getExerciseId() {
         return exerciseId;
     }
 
+    /**
+     * Gets the ID of the associated muscle group.
+     *
+     * @return the muscle group ID
+     */
     public int getMuscleGroupId() {
         return muscleGroupId;
     }
